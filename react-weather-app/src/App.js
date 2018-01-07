ffa75e46d5b1f161e00e7a9cdf9d0cec62fe4473
tony2tones <anthonymarques@outlook.com>
2018-01-07 19:21:48 +0200

    capturing coordinates

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -8,30 +8,60 @@ class App extends Component {
     error: null
   }
 
-componentDidMount = () => {
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      this.setState({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-        error: null,
-      });
-    },
-    (error) => this.setState({ error: error.message }),
-    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-  );
-}
+  componentDidMount = () => {
+    //get location, and cater for if location is provided
 
-render() {
-  return (
-    <div></div>
-    // <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
-    //   <Text>Latitude: {this.state.latitude}</Text>
-    //   <Text>Longitude: {this.state.longitude}</Text>
-    //   {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
-    // </View>
-  );
-}
+    const geoSuccess = ({ coords }) => {
+      // this.getLocation(coords);
+    };
+
+    const geoError = () => {
+      this.errorMessage = true;
+      console.log('You meant to let me have access');
+    }
+
+    if (navigator.geolocation) {
+      var gl = navigator.geolocation;
+      gl.getCurrentPosition(geoSuccess, geoError);
+    } else {
+      //if something larger than life fails
+      console.log('check yo browser out');
+    }
+
+    // const getLocation = ({ latitude, longitude }) => {
+    //   const self = this;
+    //   $.getJSON(this.apiUrl(latitude, longitude), function (data) {
+    //     self.mapData(data)
+    //   });
+    // }
+
+    // }
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Latitude: {this.state.latitude}</p>
+        <p>Longitude: {this.state.longitude}</p>
+
+      </div>
+      // <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+      //   
+      //   {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+      // </View>
+    );
+  }
 }
 
 export default App;
