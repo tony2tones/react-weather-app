3297c7da969abf5b8140e8df5bd30f6a207540b6
tony2tones <anthonymarques@outlook.com>
2018-01-07 18:31:44 +0200

    added geolocation and removed unused files

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  state = {
+    latitude: null,
+    longitude: null,
+    error: null
   }
+
+componentDidMount = () => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        error: null,
+      });
+    },
+    (error) => this.setState({ error: error.message }),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+  );
+}
+
+render() {
+  return (
+    <div></div>
+    // <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+    //   <Text>Latitude: {this.state.latitude}</Text>
+    //   <Text>Longitude: {this.state.longitude}</Text>
+    //   {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+    // </View>
+  );
+}
 }
 
 export default App;
