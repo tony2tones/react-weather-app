bb22ecdd45732c7ab922a2a9ebaff042a729ead0
tony2tones <anthony.marque@gmail.com>
2018-01-30 13:45:45 +0200

    working on the Weather icon logic

--- src/App.js
+++ src/App.js
@@ -68,26 +68,26 @@ class App extends Component {
 
   }
 
-  weatherIcon(iconName) {
-    // return iconName
-    // this.src = iconName;
-    var self = this;
-    if (iconName === '01d' || iconName === '01n') {
-        self.imageLink = "http://mikeiamele.com/wp-content/uploads/2014/05/Sunny-Day-800x450.jpg";
-    } else if (iconName === '02d' || iconName === '02n') {
-        self.imageLink = "https://www.newvision.co.ug/w-images/6f544bf7-4a4a-46a0-be3d-f656bd88bd48/1/cloudy-703x422.jpg";
-    } else if (iconName === '03d' || iconName === '03n') {
-        self.imageLink = "https://i.ytimg.com/vi/rRL_9WxBJBc/maxresdefault.jpg";
-    } else if (iconName === '04d' || iconName === '04n') {
-        self.imageLink = "http://cdn.pcwallart.com/images/dark-clouds-wallpaper-4.jpg";
-    } else if (iconName === '09d' || iconName === '09n') {
-        self.imageLink = "https://pbs.twimg.com/media/CJb-mE_VEAAu-WB.jpg";
-    } else if (iconName === '10d' || iconName === '10n') {
-        self.imageLink = "https://tctechcrunch2011.files.wordpress.com/2015/08/clouds.jpg";
-    } else if (iconName === '11d' || iconName === '11n') {
-        self.imageLink = "https://pbs.twimg.com/media/CfvNkjbWsAATGV8.jpg";
-    }
-}
+//   weatherIcon(iconName) {
+//     // return iconName
+//     this.src = iconName;
+//     var self = this;
+//     if (iconName === '01d' || iconName === '01n') {
+//         self.imageLink = "http://mikeiamele.com/wp-content/uploads/2014/05/Sunny-Day-800x450.jpg";
+//     } else if (iconName === '02d' || iconName === '02n') {
+//         self.imageLink = "https://www.newvision.co.ug/w-images/6f544bf7-4a4a-46a0-be3d-f656bd88bd48/1/cloudy-703x422.jpg";
+//     } else if (iconName === '03d' || iconName === '03n') {
+//         self.imageLink = "https://i.ytimg.com/vi/rRL_9WxBJBc/maxresdefault.jpg";
+//     } else if (iconName === '04d' || iconName === '04n') {
+//         self.imageLink = "http://cdn.pcwallart.com/images/dark-clouds-wallpaper-4.jpg";
+//     } else if (iconName === '09d' || iconName === '09n') {
+//         self.imageLink = "https://pbs.twimg.com/media/CJb-mE_VEAAu-WB.jpg";
+//     } else if (iconName === '10d' || iconName === '10n') {
+//         self.imageLink = "https://tctechcrunch2011.files.wordpress.com/2015/08/clouds.jpg";
+//     } else if (iconName === '11d' || iconName === '11n') {
+//         self.imageLink = "https://pbs.twimg.com/media/CfvNkjbWsAATGV8.jpg";
+//     }
+// }
 
   componentDidMount = () => {
     //get location, and cater for if location is provided
@@ -165,9 +165,9 @@ class App extends Component {
                 weatherNiceName={weatherNiceName}
                 cTempMax={cTempMax}
                 cTempMin={cTempMin}
-                icon={()=> this.weatherIcon(icon)}
               />
               <button class="button" onClick={this.componentDidMount}>Refresh</button>
+                {/* <img src={this.weatherIcon(this.state.weather.icon)} alt="something different" /> */}
             </div>
         }
       </div>
