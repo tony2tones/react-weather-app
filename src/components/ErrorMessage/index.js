b8061592d9f70731982ac3692efbfc4f1dc3d518
tony2tones <anthony.marque@gmail.com>
2018-02-01 16:04:51 +0200

    fixing syntax to be compliant with EsLint

--- src/components/ErrorMessage/index.js
+++ src/components/ErrorMessage/index.js
@@ -5,6 +5,6 @@ const ErrorMessage = () => (
   <div className='ErrorMessage'>
     <p> Please enable your GPS location to provide you with the latest weather updates.</p>
   </div>
-)
+);
 
 export default ErrorMessage;
