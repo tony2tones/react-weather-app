e1bef174e7690d1f51f34a2258f83e5e1c788994
tony2tones <anthony.marque@gmail.com>
2018-02-09 13:26:53 +0200

    added the icon to images component

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -4,52 +4,52 @@ import './weather.css';
 
 import Images from '../Images';
 
-const Weather = ({ location, cTemp, cTempMax, cTempMin, weatherNiceName }) => {
-  return (
-    <div className="tablestyle">
-      <hr />
-      <table className="table">
-        <thead>
-          <tr>
-            <th colSpan="4">Weather</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Location: </td>
-            <td> {location}</td>
-          </tr>
-          <tr>
-            <td>Temperature: </td>
-            <td>{cTemp}°C</td>
-          </tr>
-          <tr>
-            <td>Max Temperature: </td>
-            <td> {cTempMax}°C</td>
-          </tr>
-          <tr>
-            <td>Min Temperature: </td>
-            <td> {cTempMin}°C</td>
-          </tr>
-        </tbody>
-      </table>
-      <div>
-        <p>{weatherNiceName}</p>
-      </div>
-      <hr />
-      <div>
-        <p>Done by <a href="https://github.com/tony2tones" rel="noopener">Anthony</a></p>
-      </div>
+const Weather = ({ location, cTemp, cTempMax, cTempMin, weatherNiceName, icon }) => (
+  <div className="tablestyle">
+    <hr />
+    <table className="table">
+      <thead>
+        <tr>
+          <th colSpan="4">Weather</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Location: </td>
+          <td> {location}</td>
+        </tr>
+        <tr>
+          <td>Temperature: </td>
+          <td>{cTemp}°C</td>
+        </tr>
+        <tr>
+          <td>Max Temperature: </td>
+          <td> {cTempMax}°C</td>
+        </tr>
+        <tr>
+          <td>Min Temperature: </td>
+          <td> {cTempMin}°C</td>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      <p>{weatherNiceName}</p>
     </div>
-  );
-};
+    <hr />
+    <Images icon={icon} />
+    <div>
+      <p>Done by <a href="https://github.com/tony2tones" rel="noopener">Anthony</a></p>
+    </div>
+  </div>
+);
 
-PropTypes.checkPropTypes = {
+Weather.protoTypes = {
   location: PropTypes.string.isRequired,
   cTemp: PropTypes.string.isRequired,
   cTempMax: PropTypes.string.isRequired,
   cTempMin: PropTypes.string.isRequired,
   weatherNiceName: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
 };
 
 export default Weather;
