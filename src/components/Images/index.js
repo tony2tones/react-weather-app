540e4e4dea1e06a4885ef515f796bb4e63180e74
tony2tones <anthony.marque@gmail.com>
2018-02-12 16:24:29 +0200

    working on img icon displaying

--- src/components/Images/index.js
+++ src/components/Images/index.js
@@ -3,21 +3,15 @@ import React from 'react';
 import './images.css';
 import '../Weather';
 
-// let img = '';
-
-// const imageSwitch = (props) => {
-//   if (props.icon) {
-//     img = 'https://www.w3schools.com/html/img_girl.jpg';
-//   } else {
-//     img = '';
-//   }
-// };
-
-const Images = props => (
-  <div>
-    <img className="picture" src="https://www.w3schools.com/html/img_girl.jpg" alt="its the sun" />
-  </div>
-);
-
-
+const Images = (props) => {
+  if (props.icon === '02d') {
+    console.log(props.icon);
+    return (
+      <div className="Icon">
+        <p> you have an icon to reference {props.icon} </p>
+        <img src="https://www.ibm.com/design/assets/language/resources/icon-library/icons/svg/object-based/cloud_64.svg" alt="a cloud icon" height="42" width="42" />
+      </div>
+    );
+  }
+};
 export default Images;
