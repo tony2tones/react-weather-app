c9156577fd4fd71edf999ddf057c68799fda1a3e
tony2tones <anthony.marque@gmail.com>
2018-02-13 11:58:18 +0200

    added default value

--- src/components/Images/index.js
+++ src/components/Images/index.js
@@ -3,16 +3,20 @@ import React from 'react';
 import './images.css';
 import '../Weather';
 
+let imageSource = '../../assets/img/default.svg';
+
 const Images = (props) => {
   if (props.icon === '01d') {
-    let imageSource = require('../../assets/img/sun.svg');
-    if (props.icon === '02d') {
-      imageSource = require('../../assets/img/cloud_64.svg');
-    } return (
-      <div>
-        <img src={imageSource} alt="a cloud icon" />
-      </div>
-    );
+    imageSource = require('../../assets/img/sun.svg');
+  } else if (props.icon === '02d') {
+    imageSource = require('../../assets/img/cloud_64.svg');
+  } else if (props.icon === '03d') {
+    imageSource = require('../../assets/img/cloud_64.svg');
   }
+  return (
+    <div>
+      <img src={imageSource} alt="a cloud icon" />
+    </div>
+  );
 };
 export default Images;
