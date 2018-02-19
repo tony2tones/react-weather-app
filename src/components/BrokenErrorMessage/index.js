0fbb6e318000d43880c8273c93b2988cd0df488d
tony2tones <anthonymarques@outlook.com>
2018-02-19 19:33:43 +0200

    adding broken message when all goes wrong

---
+++ src/components/BrokenErrorMessage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './brokenErrorMessage.css';
+
+const BrokenErrorMessage = () => (
+  <div className="ErrorMessage">
+    <p>Please enable your GPS location to provide you with the latest weather updates.</p>
+  </div>
+);
+
+export default BrokenErrorMessage;
