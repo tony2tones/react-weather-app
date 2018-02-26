b5a844ec09dd9237f07ddc3755c6aa8e30f06e57
tony2tones <anthony.marque@gmail.com>
2018-02-26 11:24:51 +0200

    updated  package.json and updated test script

--- src/components/Weather/Weather.test.js
+++ src/components/Weather/Weather.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
 import { shallow } from 'enzyme';
 // import Adapter from 'enzyme-adapter-react-16';
 
 import Weather from './index';
+import register from 'ignore-styles';
+register(['.sass', '.scss']);
 
 // configure({ adapter: new Adapter() });
 
 describe('<Weather />', () => {
+  let wrapper;
+  beforeEach(() => { wrapper = shallow(<Weather />); });
+
   it('should render something cool...still practicing at the mo', () => {
-    const wrapper = shallow(<Weather />);
-    expect(wrapper.find('className').getText('.iconHeader')).toHaveLength(1);
+    expect(wrapper.find('div.iconHeader')).to.Have.Length(1);
   });
 });
