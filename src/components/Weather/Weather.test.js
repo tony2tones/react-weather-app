760c9515a9bfaaf66c644dce5f9d6747b1e0029e
tony2tones <anthony.marque@gmail.com>
2018-02-26 11:41:26 +0200

    updated test script

--- src/components/Weather/Weather.test.js
+++ src/components/Weather/Weather.test.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
 
 import Weather from './index';
-import register from 'ignore-styles';
-register(['.sass', '.scss']);
-
-// configure({ adapter: new Adapter() });
 
 describe('<Weather />', () => {
   let wrapper;
