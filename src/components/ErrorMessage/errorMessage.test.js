653bd7a59dff9a3e97f76c8430e2bebd43734554
tony2tones <anthony.marque@gmail.com>
2018-03-20 13:40:40 +0200

    updated unit tests to streamline tests and fixed spelling error.

--- src/components/ErrorMessage/errorMessage.test.js
+++ src/components/ErrorMessage/errorMessage.test.js
@@ -3,15 +3,6 @@ import { shallow } from 'enzyme';
 
 import ErrorMessage from './index';
 
-test('should render the error message without crashing ', () => {
-  const wrapper = shallow(<ErrorMessage />);
-  const expected = true;
-
-  const actual = wrapper.find('.ErrorMessage').exists();
-
-  expect(actual).toBe(expected);
-});
-
 test('should have the following error message', () => {
   const wrapper = shallow(<ErrorMessage />);
   const expected = 'Please enable your GPS location to provide you with the latest weather updates.';
